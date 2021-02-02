9419edb106621c55786c40188c70d544d2f644ac
Peter Ardiarta <50816765+prooholic@users.noreply.github.com>
2021-02-02 18:17:00 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Prooholic Kranul by Prooholic @telegram
+kernel.string=LoliKernel by Prooholic@telegram 
 do.devicecheck=1
 do.modules=0
 do.systemless=1
